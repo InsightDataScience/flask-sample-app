8928c92604bb506c37c5925b57da4102c3c0332a
Thomas Mathew <mothas.thomas@gmail.com>
2020-02-04 23:45:43 -0800

    Update windowScript.js

--- static/js/windowScript.js
+++ static/js/windowScript.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
     $('#btn-hide-text').click(function() { $('#additional_text').hide() } );
     $('#btn-show-text').click(function() { $('#additional_text').show() } );
     $('#btn-show-route').click(function() {
-      $.get("/user/super_user", function(data, status) {
+      var userName = $('#user_name').val(),
+          uri = "/user/" + userName;
+      $.get(uri, function(data, status) {
         console.log('blah 1 ' + JSON.stringify(data));
-        console.log('blah 2 ' + $('#user_name').val());
       });
     });
 });
