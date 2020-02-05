db393a8b36912cfde3893919fe663efac14cc2c2
Thomas Mathew <mothas.thomas@gmail.com>
2020-02-04 23:15:50 -0800

    Update windowScript.js

--- static/js/windowScript.js
+++ static/js/windowScript.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
     $('#btn-hide-text').click(function() { $('#additional_text').hide() } );
     $('#btn-show-text').click(function() { $('#additional_text').show() } );
     $('#btn-show-route').click(function() {
