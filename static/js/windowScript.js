31e1d2b83ec95b22cbb98eb4b1627d1aad089010
Thomas Mathew <mothas.thomas@gmail.com>
2020-02-04 23:06:42 -0800

    Include debug

--- static/js/windowScript.js
+++ static/js/windowScript.js
@@ -2,6 +2,7 @@ $(function() {
     $('#btn-hide-text').click(function() { $('#additional_text').hide() } );
     $('#btn-show-text').click(function() { $('#additional_text').show() } );
     $('#btn-show-route').click(function() {
+      console.log('blah 1');
       $.get("user/super_user", function(data, status) {
         console.log('blah' + JSON.stringify(data));
       });
