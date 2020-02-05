7c467cd0cf20618554736afdefc53439bd074da1
Thomas Mathew <mothas.thomas@gmail.com>
2020-02-04 23:03:14 -0800

    Include route link

--- static/js/windowScript.js
+++ static/js/windowScript.js
@@ -1,4 +1,9 @@
 $(function() {
     $('#btn-hide-text').click(function() { $('#additional_text').hide() } );
     $('#btn-show-text').click(function() { $('#additional_text').show() } );
+    $('#btn-show-route').click(function() {
+      $.get("user/super_user", function(data, status) {
+        console.log('blah' + JSON.stringify(data));
+      });
+    });
 });
